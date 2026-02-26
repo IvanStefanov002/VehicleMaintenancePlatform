21ac7272a9551b8cc743cc8fece03751e05d0d46
IvanStefanov002 <91614351+IvanStefanov002@users.noreply.github.com>
2026-02-26 15:04:39 +0200

    Delete CarMaintenanceFE directory

--- CarMaintenanceFE/auth.js
+++
@@ -1,7 +0,0 @@
-/* function used to lock pages and init authorization */
-function requireAuth() {
-    const user = localStorage.getItem("loggedUser");
-    if (!user) {
-        window.location.replace("/index.html");
-    }
-}
